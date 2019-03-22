9fa6dff06ee6c92f695e6a671d22cc0cc29dca0d
Suraj <sbsurajbhat@gmail.com>
2019-03-22 21:31:55 +0530

    Live Bitcoin Price Fetching

--- ProjectUI/app.py
+++ ProjectUI/app.py
@@ -1,12 +1,16 @@
 from flask import Flask,render_template
+import requests
 
 app = Flask(__name__)
 
 
+r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+price = r.json()['bpi']['USD']['rate']
+
 @app.route('/')
-@app.route('/home')
+@app.route('/home/<float:price>')
 def index():
-    return render_template("home.html")
+    return render_template("home.html", price=price)
 
 if(__name__=="__main__"):
     app.run(debug=True)
